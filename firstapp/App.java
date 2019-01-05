858a37b17c596764dd05b8df09cec257c9a49499
Scott Morse <scottmorsedev@gmail.com>
2019-01-04 20:46:46 -0600

    Wrote my first Java\!

--- firstapp/App.java
+++ firstapp/App.java
@@ -1,5 +1,17 @@
 public class App {
   public static void main(String[] args){
     System.out.println("What hath God wrought?");
+    int x = 0;
+    while (x < 10){
+      x++;
+      System.out.println(x);
+    }
+    for (int i = 0; i < 10; i++){
+      System.out.println(Math.round(Math.random() * 10));
+    }
+    // comment
+    /* 
+      comment 
+    */
   }
 }
